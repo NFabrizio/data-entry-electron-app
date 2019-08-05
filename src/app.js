e7ba7d2b44b3fd6c387dc0c0802ee4f0df58ce48
NFabrizio <nick.fabrizio@pearson.com>
2019-08-05 09:42:54 -0600

    Adds README and updates app.js to pure functional component

--- src/app.js
+++ src/app.js
@@ -1,14 +1,12 @@
 import React from 'react';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <div className="hello">
-        <h2>Hello Basic Electron App</h2>
-        <img src="assets/Logo.png" />
-        <h4>A basic Electron + React.js template</h4>
-        <h4>Have Fun!</h4>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="hello">
+    <h2>Hello Basic Electron App</h2>
+    <img src="assets/Logo.png" />
+    <h4>A basic Electron + React.js template</h4>
+    <h4>Have Fun!</h4>
+  </div>
+);
+
+export default App;
