654d8f966947accda1e704de8691524caae30649
NFabrizio <nicholas.fabrizio@gmail.com>
2019-08-05 08:58:34 -0600

    Cleans up code styles

--- src/app.js
+++ src/app.js
@@ -1,15 +1,14 @@
 import React from 'react';
 
 export default class App extends React.Component {
-    render() {
-        return (
-            <div className={'hello'}>
-                <h2>Hello Basic Electron App</h2>
-                <img src="assets/Logo.png" />
-                <h4>A basic Electron + React.js template</h4>
-                <h4>Have Fun!</h4>
-            </div>
-
-        );
-    }
+  render() {
+    return (
+      <div className="hello">
+        <h2>Hello Basic Electron App</h2>
+        <img src="assets/Logo.png" />
+        <h4>A basic Electron + React.js template</h4>
+        <h4>Have Fun!</h4>
+      </div>
+    );
+  }
 }
